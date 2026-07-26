e62841cbad87260b6dbb3efdfaad8c3cb409adbb
evan-mulumba <evan.mulumba.dev@gmail.com>
2026-07-26 22:44:43 +0200

    Test component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import {Home} from './components/home/home';
-import {MyList} from './components/my-list/my-list';
+import { Home } from './components/home/home';
+import { MyList } from './components/my-list/my-list';
+import { Test } from './components/test/test';
 
 export const routes: Routes = [
   { path: '', title: 'Home', component: Home },
   { path: 'my-list', title: 'My List', component: MyList },
+  { path: 'test', title: 'Test', component: Test },
 ];
