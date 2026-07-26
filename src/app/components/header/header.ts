127a39b4af66e48afdb4da0bfc000519a5e440ba
evan-mulumba <evan.mulumba.dev@gmail.com>
2026-07-26 19:15:34 +0200

    Header - Logo

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainNav } from '../main-nav/main-nav';
 import { LoginAction } from '../login-action/login-action';
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-header',
-  imports: [MainNav, LoginAction],
+  imports: [MainNav, LoginAction, Logo],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   standalone: true,
