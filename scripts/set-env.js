be6a294e84150c355f76d55bd3c119678708e524
evan-mulumba <evan.mulumba.dev@gmail.com>
2026-08-30 19:03:53 +0200

    Config - env var

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -7,7 +7,9 @@ const envConfigFile = `export const environment = {
   production: true,
   rawgApiKey: ${JSON.stringify(process.env.RAWG_API_KEY || "EMPTY_ENV_VAR")},
   apiUrl: ${JSON.stringify(process.env.API_URL || "EMPTY_ENV_VAR")},
-  version_tag: ${JSON.stringify(process.env.VERSION_TAG || "v0.0.0")}
+  version_tag: ${JSON.stringify(process.env.VERSION_TAG || "v0.0.0")},
+  googleClientId: ${JSON.stringify(process.env.GOOGLE_CLIENT_ID || "EMPTY_ENV_VAR")},
+  googleClientSecret: ${JSON.stringify(process.env.GOOGLE_CLIENT_SECRET || "EMPTY_ENV_VAR")},
 };
 `;
 
