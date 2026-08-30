b8416b031502deec5c5989e7d6cc4867b2dc87f1
evan-mulumba <evan.mulumba.dev@gmail.com>
2026-08-30 22:08:26 +0200

    Routing - tab title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { Test } from './components/test/test';
 import { Backlog } from './components/backlog/backlog';
 import { MainLayoutComponent } from './components/main-layout-component/main-layout-component';
+import { Test } from './components/test/test';
 
 export const routes: Routes = [
-  { path: '', title: 'MainLayoutComponent', component: MainLayoutComponent },
-  { path: 'backlog', title: 'Backlog', component: Backlog },
+  { path: '', title: 'Endgame', component: MainLayoutComponent },
+  { path: 'backlog', title: 'Endgame - Backlog', component: Backlog },
   { path: 'test', title: 'Test', component: Test },
 ];
